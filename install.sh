9c8ef173eb27993e90b0058bd4fc0f86b7f8e07c
mathlon26 <mathijs.follon@gmail.com>
2025-03-18 10:46:32 +0100

    install.sh and update

--- install.sh
+++ install.sh
@@ -87,6 +87,7 @@ if [[ "$continueInstall" =~ ^[Yy] ]]; then
                 mkdir -p "$HOME/.config/devcore/templates/$lang"
                 for template_dir in "templates/$lang"/*; do
                     if [ -d "$template_dir" ]; then
+                        pwd
                         echo "$template_dir" "$HOME/.config/devcore/templates/$lang/"
                         cp -r "$template_dir" "$HOME/.config/devcore/templates/$lang/"
                     fi
